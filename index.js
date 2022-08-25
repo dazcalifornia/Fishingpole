b99fc317823ceda181b4bdd1f02dab948dbf68ab
Supakit Wiwustrinon <franx@Supakits-MacBook-Air.local>
2022-08-25 17:35:09 +0700

    cleaning for startproject

--- index.js
+++ index.js
@@ -2,11 +2,31 @@ const express = require('express');
 const app = express();
 const cors = require("cors");
 const axios = require("axios");
+const bodyParser = require('body-parser')
 
+require('dotenv').config();
 
 app.use(cors());
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+
+app.post('/', (req, res) => {
+  res.sendStatus(200);
+})
+
+app.post ('/webhook', (req, res) => {
+  const { body } = req;
+  console.log(body);
+  res.sendStatus(200);
+})
 
 const port = process.env.environment === 'server' ? 80 : 3505;
 const server = app.listen(port,  function(){
   console.log('server listen port :'+port);
 })
+
+
